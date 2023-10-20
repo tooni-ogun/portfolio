7cd6f4c77aa43b065ae2033561cdedde06972c46
tooni-ogun <118496897+tooni-ogun@users.noreply.github.com>
2023-10-20 18:22:12 -0400

    Add files via upload

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title><?php echo $title; ?></title>
+  <link rel="stylesheet" type="text/css" href="/public/styles/site.css" media="all">
+</head>
+
+<body>
+<h1> hi </h1>
+</body>
+</html>
